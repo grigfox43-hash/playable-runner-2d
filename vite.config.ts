ebbb35cf2dd53137c557852740b2c88b025700f4
xgr3enx <148909304+xgr3enx@users.noreply.github.com>
2026-08-19 13:54:47 +0300

    chore: setup project structure, build tools and configurations

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+export default defineConfig({
+  plugins: [viteSingleFile()],
+  build: {
+    target: 'es2020',
+    minify: 'esbuild',
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
